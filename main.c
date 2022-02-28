d78ead49890c3dfd45ee113839d0baf57e72bb34
Lee Essen <lee.essen@nowonline.co.uk>
2022-02-28 17:34:48 +0000

    Reworked DMA/IRQ code

--- main.c
+++ main.c
@@ -82,29 +82,95 @@ void my_clocks_init(void) {
                     100 * MHZ);
 }
 
-int rx_dma_chan;
 
-uint8_t rx_frame[2048];
+PIO                 rx_pio = pio0;          // Which PIO are we running on for RX
+uint                rx_sm;                 // State machine for RX
+uint                rx_offset;             // Start of the RX code
+
+dma_channel_hw_t    *rx_dma_chan_hw;
+int                 rx_dma_chan;
+uint8_t             rx_frame[2048];
 
 #define RX_DMA_LENGTH         1200
 
 volatile int flag = 0;
 
 
+//
+// This is called when the state machine reaches the end of a packet
+//
+// So ... stop the sm, clear the sm irq, abort the dma, then setup
+// everything new again.
+//
+// This is a higher priority isr, so there could be a dma IRQ pending
+// which we need to clear, we should also avoid creating one by aborting
+// the dma.
+//
+// Suggestion: before aborting the dma turn off the DMA irq.
+//
+//
+void pio_rx_isr() {
+    uint32_t length;
 
-void isr()
-{
-    //printf("In here");
+    // First disable the DMA irq from happening when we abort...
+    dma_channel_set_irq0_enabled(rx_dma_chan, false);
+
+    // Now abort the DMA channel... (this won't wait for outstanding xfers)
+    dma_hw->abort = 1u << rx_dma_chan;
+
+    // Stop the state machine...
+    pio_sm_set_enabled(rx_pio, rx_sm, false);
+
+    // Clear SM IRQ (irq0 rel)
+    pio_interrupt_clear(pio0, rx_sm + 0);
+    //irq_clear(PIO0_IRQ_0)
+
+    // Get packet size....
+    length = (uint32_t)rx_dma_chan_hw->write_addr - (uint32_t)rx_frame;
+
+    // Wait for abort to complete
+    while (dma_hw->abort & (1ul << rx_dma_chan)) tight_loop_contents();
+
+    // Reconfigure and re-start DMA
+    dma_channel_acknowledge_irq0(rx_dma_chan);
+    irq_clear(DMA_IRQ_0);   
+    dma_channel_hw_addr(rx_dma_chan)->write_addr = (uintptr_t)rx_frame;
+    dma_channel_hw_addr(rx_dma_chan)->al1_transfer_count_trig = RX_DMA_LENGTH;
+    dma_channel_set_irq0_enabled(rx_dma_chan, true);
+
+    // Start SM (jmp to offset)
+    pio_sm_restart(rx_pio, rx_sm);
+    pio_sm_clkdiv_restart(rx_pio, rx_sm);
+    pio_sm_exec(rx_pio, rx_sm, pio_encode_jmp(rx_offset));
+    pio_sm_set_enabled(rx_pio, rx_sm, true);
+
+    printf("PIO_RX_ISR (size=%d)\r\n", length);
+
     // State machine number impacts the IRQ...
-    pio_interrupt_clear(pio0, 0);
-    pio_interrupt_clear(pio0, 1);
-    irq_clear(PIO0_IRQ_0);
-    flag = 1;
+//    pio_interrupt_clear(pio0, 0);
+//    pio_interrupt_clear(pio0, 1);
+//    irq_clear(PIO0_IRQ_0);
+//    flag = 1;
 }
 
+//
+// We should only get a dma IRQ now if we have overrun the size limit, however
+// there is the possiblity that the SM finishes while we are in here and
+// pre-empts us ... 
+// disable_irqs(); set PIO irq flags; enable_irqs();
+//
+// Does that fix the problem?
+//
 void dma_isr() {
+    // Is it easier to just trigger a PIO irq?
+    // Either by setting the flag manually or by executing a
+    // command on the SM? Need to think.
+    dma_channel_hw_t *hw = dma_channel_hw_addr(rx_dma_chan);
+
     dma_channel_acknowledge_irq0(rx_dma_chan);
-    printf("DMA OVERRUN");
+
+    uint32_t bytes = (uint32_t)hw->write_addr - (uint32_t)rx_frame;
+    printf("DMA OVERRUN bytes=%d\r\n", bytes);
 }
 
 int main() {
@@ -122,9 +188,9 @@ int main() {
     //gpio_set_slew_rate(21, GPIO_SLEW_RATE_FAST);
     clock_gpio_init(21, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS, 10);
 
-    PIO pio = pio0;
-    uint offset;
-    uint sm;
+    //PIO pio = pio0;
+    //uint offset;
+    //uint sm;
 
 /*    
     offset = pio_add_program(pio, &clock_program);
@@ -161,8 +227,8 @@ int main() {
     //
     // Now the RX side
     //
-    offset = pio_add_program(pio, &macrx_program);
-    sm = pio_claim_unused_sm(pio, true);
+    rx_offset = pio_add_program(rx_pio, &macrx_program);
+    rx_sm = pio_claim_unused_sm(rx_pio, true);
 
     // We need pull downs enabled on the rx pins, I assume this is to ensure it
     // gets back to 0 quickly enough.
@@ -177,7 +243,7 @@ int main() {
     gpio_pull_down(28);
 
     // this one doesn't start it...
-    macrx_program_init(pio, sm, offset, 26, 28);    // rx0=26, rx1=27, csr=28
+    macrx_program_init(rx_pio, rx_sm, rx_offset, 26, 28);    // rx0=26, rx1=27, csr=28
 
 
     // We have a DMA complete interrupt which will signal if we overrun a buffer
@@ -191,6 +257,7 @@ int main() {
 
     rx_dma_chan = dma_claim_unused_channel(true);
     rx_dma_channel_config = dma_channel_get_default_config(rx_dma_chan);
+    rx_dma_chan_hw = dma_channel_hw_addr(rx_dma_chan);
 
     irq_set_exclusive_handler(DMA_IRQ_0, dma_isr);
     irq_set_enabled(DMA_IRQ_0, true);
@@ -201,31 +268,33 @@ int main() {
         
     channel_config_set_read_increment(&rx_dma_channel_config, false);
     channel_config_set_write_increment(&rx_dma_channel_config, true);
-    channel_config_set_dreq(&rx_dma_channel_config, pio_get_dreq(pio, sm, false));
+    channel_config_set_dreq(&rx_dma_channel_config, pio_get_dreq(rx_pio, rx_sm, false));
     channel_config_set_transfer_data_size(&rx_dma_channel_config, DMA_SIZE_8);
 
     dma_channel_configure(
         rx_dma_chan, &rx_dma_channel_config,
         rx_frame,
-        ((uint8_t*)&pio->rxf[sm]) + 3,
+        ((uint8_t*)&rx_pio->rxf[rx_sm]) + 3,
         RX_DMA_LENGTH,
         false
     );
 
-    irq_set_exclusive_handler(PIO0_IRQ_0, isr);
+    irq_set_exclusive_handler(PIO0_IRQ_0, pio_rx_isr);
     irq_set_enabled(PIO0_IRQ_0, true);
 
 
     dma_channel_start(rx_dma_chan);
-    pio_sm_set_enabled(pio, sm, true);
+    pio_sm_set_enabled(rx_pio, rx_sm, true);
+
+    while(1);
 
     dma_channel_hw_t *hw = dma_channel_hw_addr(rx_dma_chan);
     while(1) {
         uint64_t now = time_us_64();
         while(!flag) {
             if (time_us_64() > now + 3000000) {
-                int pc = pio_sm_get_pc(pio, sm);
-                printf("No data in 3s ... offset=%d pc=%d\r\n", offset, pc);
+                int pc = pio_sm_get_pc(rx_pio, rx_sm);
+                printf("No data in 3s ... offset=%d pc=%d\r\n", rx_offset, pc);
                 printf("rx_frame=%08x rx_addr=%08x\r\n", rx_frame, hw->write_addr);
                 now = time_us_64();
             }
@@ -239,7 +308,7 @@ int main() {
             sleep_us(10);
         }
         printf("\r\n");
-        pio_sm_set_enabled(pio, sm, false);
+        pio_sm_set_enabled(rx_pio, rx_sm, false);
 
         uint32_t rxaddr = hw->write_addr;
         int len = rxaddr - ((uint32_t)rx_frame);
@@ -258,7 +327,7 @@ int main() {
         dma_channel_configure(
             rx_dma_chan, &rx_dma_channel_config,
             rx_frame,
-            ((uint8_t*)&pio->rxf[sm]) + 3,
+            ((uint8_t*)&rx_pio->rxf[rx_sm]) + 3,
             RX_DMA_LENGTH,
             false
         );
@@ -267,10 +336,10 @@ int main() {
         dma_hw->sniff_data = 0xffffffff;
 
         dma_channel_start(rx_dma_chan);
-        macrx_program_init(pio, sm, offset, 26, 28);    // rx0=26, rx1=27, csr=28
+        macrx_program_init(rx_pio, rx_sm, rx_offset, 26, 28);    // rx0=26, rx1=27, csr=28
 
         flag = 0;
-        pio_sm_set_enabled(pio, sm, true);
+        pio_sm_set_enabled(rx_pio, rx_sm, true);
     }
 
     while(1);
@@ -281,8 +350,8 @@ int main() {
         printf("At location: %02x --> %02x\r\n", i, rx_frame[i]);
     }
 
-    printf("Offser is %d\r\n", offset);
-    uint8_t pc = pio_sm_get_pc(pio, sm);
+    printf("Offser is %d\r\n", rx_offset);
+    uint8_t pc = pio_sm_get_pc(rx_pio, rx_sm);
     printf("Pc is %d\r\n", pc);
 
     dma_channel_config xx = dma_get_channel_config(rx_dma_chan);
